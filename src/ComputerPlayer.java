3ec4fd0a7dbdbe3d5726e08e65691526f2d76f1a
smz785 <20480538@fhda.edu>
2026-03-03 00:42:51 -0800

    Updated ComputerPlayer.java

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,10 +1,19 @@
+import java.util.Random;
+
 /**
  * Implements Coding To Interface Design Principle
  * @author SyedZain
  */
 public class ComputerPlayer implements Player {
+
+    private String[] choice = {"rock", "paper", "scissor"};
+
     @Override
     public String getChoice() {
-        return "";
+        int min = 0;
+        int max = choice.length - 1;
+        Random random = new Random();
+        int randomNum = random.nextInt(max - min + 1) + min;
+        return choice[randomNum];
     }
 }
